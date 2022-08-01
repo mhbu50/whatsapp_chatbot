3bbca7b84a6d54501b189430d46d804869615f50
mhbu50 <mhbu50@gmail.com>
2022-08-01 23:53:13 +0000

    fix user datamap loop

--- whatsapp_chatbot/whatsapp_chatbot/tools.py
+++ whatsapp_chatbot/whatsapp_chatbot/tools.py
@@ -14,6 +14,21 @@ def download_pdf(doctype, name, format=None, doc=None, no_letterhead=0):
 	html = frappe.get_print(doctype, name, format, doc=doc, no_letterhead=no_letterhead)
 	return get_pdf(html)
 
+# def get_users_next_action_data(transitions, doc):
+# 	user_data_map = []
+# 	for transition in transitions:
+# 		users = get_users_with_role(transition.allowed)
+# 		filtered_users = filter_allowed_users(users, doc, transition)
+# 		for fu in filtered_users:
+# 			user = frappe._dict({'possible_actions': [],'email': frappe.db.get_value('User', fu, 'email')})
+# 			user_data_map.append(user)
+
+# 			# user_data_map.get('possible_actions').append(frappe._dict({
+# 			# 	'action_name': transition.action,
+# 			# 	'action_link': get_confirm_workflow_action_url(doc,transition.action,  user)
+# 			# }))
+# 	return user_data_map
+
 def get_users_next_action_data(transitions, doc):
 	user_data_map = {}
 	for transition in transitions:
@@ -44,28 +59,19 @@ def send_whatsapp(doc, state):
 	user_data_map = get_users_next_action_data(next_possible_transitions, doc)
 	if not user_data_map: return
 	print("\nuser_data_map = {}\n".format(user_data_map))
-	# user_list = {}
+	user_list = []
 	for user in user_data_map:	
 		user_doc = frappe.get_doc("User",user)
 		user_data_map[user]["first_name"] = user_doc.first_name
 		user_data_map[user]["mobile_no"] = user_doc.mobile_no
-		user_data_map[user]["mobile_no"] = user_doc.mobile_no
 		user_data_map[user]["doctype"] = doc.get('doctype')
 		user_data_map[user]["docname"] = doc.get('name')
-		# user_list.update({
-		# 	"first_name":user_doc.first_name,
-		# 	"mobile_no": user_doc.mobile_no,
-		# 	"possible_actions":user_data_map[user]["possible_actions"]
-		# 	})
-
+		user_list.append(user_data_map)
 	# print("user_data_map = {}".format(frappe.as_json(user_data_map)))
-
-
 	url_host = frappe.db.get_single_value('Chatbot Settings','host_url')
 	headers = {"Content-Type": "application/json; charset=UTF-8" }
-	data = json.dumps(user_data_map)
-	# res = make_post_request(url = url_host + "/send-workflow", data= user_data_map  )
-	resp = req.post(url_host + "/send-workflow", data, headers=headers)
+	data = json.dumps(user_list)
+	req.post(url_host + "/send-workflow", data, headers=headers)
 
 	
 
